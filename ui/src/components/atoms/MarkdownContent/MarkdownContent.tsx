7af359200f8454c4b264402fffc5b5d3f4bc7629
tamai-hideyuki <hide27yuki@gmail.com>
2025-12-23 11:11:32 +0900

    remark-breaks プラグインを追加しました。これにより、単一の改行が <br> タグに変換

--- ui/src/components/atoms/MarkdownContent/MarkdownContent.tsx
+++ ui/src/components/atoms/MarkdownContent/MarkdownContent.tsx
@@ -1,5 +1,6 @@
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import remarkBreaks from 'remark-breaks'
 import './MarkdownContent.css'
 
 type MarkdownContentProps = {
@@ -10,7 +11,7 @@ type MarkdownContentProps = {
 export const MarkdownContent = ({ content, className = '' }: MarkdownContentProps) => {
   return (
     <div className={`markdown-content ${className}`.trim()}>
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm, remarkBreaks]}>{content}</ReactMarkdown>
     </div>
   )
 }
